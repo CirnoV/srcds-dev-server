6fbd3a1540f5bb8316833093e732a9f9860b62af
CirnoV <cirnov59@gmail.com>
2021-11-27 18:49:33 +0900

    v0.1.1

--- build.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const child_process = require('child_process');
-
-child_process.execSync('cargo build --release', { stdio: 'inherit' });
-fs.copyFileSync('target/release/touhou-dev-server.exe', '../touhou-dev-server.exe');
